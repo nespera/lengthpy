cc78afc22961f5853cf51e01046971178a0e5903
Chris Rimmer <c@24.org.uk>
2013-01-15 20:31:43 +0000

    Extra tests

--- tests.py
+++ tests.py
@@ -13,6 +13,8 @@ class TestLength(unittest.TestCase):
 
     def test_lengths_have_user_friendly_string_rep(self):
         self.assertEqual(str(Length(2.3, YARD)), '2.30 yd')
+        self.assertEqual(str(Length(0.3, METRE)), '0.30 m')
+        self.assertEqual(str(Length(38, INCH)), '38.00 in')
 
 if __name__ == '__main__':
     unittest.main()
